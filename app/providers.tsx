8338feda5241b999988799387b83dec7243ac48c
Krish Murjani <krishmurjani2@gmail.com>
2026-03-06 15:29:54 -0500

    chore: remove FloatingProgressOverlay step tracker

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
-import FloatingProgressOverlay from "@/components/FloatingProgressOverlay";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
       {children}
-      <FloatingProgressOverlay />
     </HeroUIProvider>
   );
 }
